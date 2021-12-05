35e8de36b708d842652ec8bffc159d5a93408f1e
Bartosz Sroka <srokab019@gmail.com>
2021-12-05 18:36:53 +0100

    move class Specimen to another file

---
+++ specimen.py
@@ -0,0 +1,52 @@
+import random
+
+import numpy as np
+
+import functions
+
+
+class Specimen:
+    def __init__(self, matrix: np.ndarray):
+        self.size = matrix.shape[0]
+        self.rows = np.sum(matrix, axis=1)
+        self.cols = np.sum(matrix, axis=0)
+        self.elite = False
+        self.matrix = matrix
+
+    def initialize_matrix_permutation(self):
+        cols, rows = self.cols, self.rows
+        matrix = np.zeros_like(self.matrix)
+        while list(np.sum(matrix, axis=0)) != list(cols):
+            temp_columns = cols
+            for row in range(self.size - 1):
+                permutation = list(functions.sums(self.size, rows[row]))
+                rows_ = functions.filter(permutation, temp_columns, row)
+                if len(rows_) == 0:
+                    continue
+                idx_random_row = np.random.randint(len(rows_))
+                matrix[row] = rows_[idx_random_row]
+                temp_columns = [temp_columns[i] - matrix[row][i] for i in range(self.size)]
+            if temp_columns[-1] != 0:
+                continue
+            matrix[-1] = temp_columns
+        self.matrix = matrix
+
+    def initialize_matrix_change(self):
+        while np.sum(self.matrix.diagonal()) != 0:
+            visit = [(row, col) for row in range(self.size) for col in range(self.size)]
+            matrix = np.zeros_like(self.matrix)
+            rows, cols = self.rows, self.cols
+            while visit:
+                row, col = random.choice(visit)
+                val = min(rows[row], cols[col])
+                matrix[row][col] = val
+                rows[row] = rows[row] - val
+                cols[col] = cols[col] - val
+                visit.remove((row, col))
+            self.matrix = matrix
+
+    def quality(self):
+        return np.count_nonzero(self.matrix == 0) - self.size
+
+    def display(self):
+        print(self.matrix, "\n")
